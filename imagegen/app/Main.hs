f5b1eca6fd3bc40ec610945a5b9b3c1ab05ee6d1
Jonathan Merritt <j.s.merritt@gmail.com>
2021-07-04 16:38:27 +1000

    Image generation and math test

--- imagegen/app/Main.hs
+++ imagegen/app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Diagrams.Prelude
+import Diagrams.Backend.SVG
+
+b1 :: Diagram B
+b1 = square 20 # lw 0.002 # fc blue
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Hello, Haskell!"
+  renderSVG "../doc/img/square.svg" (dims (V2 400 400)) b1
